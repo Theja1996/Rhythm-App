6ce02409e0c8d6ffba0ca06a574a3e8bc764d9df
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-11-22 13:08:24 +0530

    fetch data from API

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,37 +4,38 @@
       <form>
         <fieldset>
           <legend>Legend</legend>
-          <div class="form-group">
-            <label for="exampleInputmusic">music</label>
-            <div class="col-md-10">
-              <input
-                type="music"
-                class="form-control"
-                id="exampleInputmusic"
-                aria-describedby="music"
-                placeholder="search music"
-              />
-            </div>
-          </div>
+          <app-search @newDataSet="result = $event"></app-search>
         </fieldset>
       </form>
     </div>
+
     <div class="row">
-        <div class="col-md-12">
-        Result
-        </div>
+      <div class="col-md-12">
+        <app-result  :reList= "result"></app-result>
+        
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import SearchArea from "./SearchArea";
+import ReasulthArea from "./ResultArea";
+export default {
+
+data(){
+  return{
+result:[]
+  }
+},
+
+
+
+  components: {
+    "app-search": SearchArea,
+    "app-result": ReasulthArea
+  }
+};
 </script>
 
-<style>
-.exampleinputmusic: {
-  align-items: center;
-  justify-content: center;
-  margin-top: 20px;
-}
-</style>
+<style></style>
